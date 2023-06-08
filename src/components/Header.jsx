2097c1fed7d9a6802a50d85018bc581f648e36c1
keneroli18 <129073693+keneroli18@users.noreply.github.com>
2023-06-08 10:11:37 -0500

    actividad7

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./mensaje.css"
+
+const Header = () => {
+    return (
+       <>
+            <header>HOLA BRO</header>
+       </>
+    );
+};
+
+export default Header; 
